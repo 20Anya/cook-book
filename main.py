69dea6ff43815862d195c66dd055824d16f75d91
Anya <aana45751@gmail.com>
2024-09-15 15:25:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,17 +16,19 @@ with open('cook_book.txt', encoding="utf-8") as file:
 def get_shop_list_by_dishes(dishes, person_count):
     new_book = {}
     for dish in dishes:
-      for dishes_name in cook_book:
-        if dish in dishes_name:
+        if dish in cook_book:
           for ingredients in cook_book[dish]:
             ingredient_name1 = ingredients.get('ingredient')
             quantity1 = int(ingredients.get('quantity')) * person_count
             measure1 = ingredients.get('measure')
-            new_book1 = {ingredient_name1: {'quantity': quantity1, 'measure': measure1}}
-            new_book.update(new_book1)
+            if ingredient_name1 in new_book:
+                quantity1 += new_book[ingredient_name1]['quantity']
+            new_book[ingredient_name1] = {'quantity': quantity1, 'measure': measure1}
+        else:
+            print(f'Блюда {dish} нет в книге рецептов')
     print(new_book)
 
-get_shop_list_by_dishes(['Запеченный картофель','Омлет'], 5)
+get_shop_list_by_dishes(['Омлет', 'Омлет'], 2)
 
 
 
